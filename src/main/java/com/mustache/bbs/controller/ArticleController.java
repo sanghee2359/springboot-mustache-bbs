3428c953dcf545db29119297abf582cd2a9763ec
sanghee <wjdtkdgml7352@daum.net>
2022-11-07 16:11:05 +0900

    Repository 인터페이스 생성 - Dao의 추상화 ver 1. ArticleController에 repository를 DI 2. application.yml에 db 접속 정보 설정

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.mustache.bbs.controller;
 
+import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +14,25 @@ import com.mustache.bbs.domain.dto.ArticleDto;
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping(value="/new")
     public String newArticleForm() {
         return "articles/new";
     }
     @PostMapping(value="/posts")
     public String createArticle(ArticleDto form) {
+        // Dto를 Entity로 변환
+        Article article = form.toEntity();
         log.info(form.toString());
+
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+
         return "";
     }
 
