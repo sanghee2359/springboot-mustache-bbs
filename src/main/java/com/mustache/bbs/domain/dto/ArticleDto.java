dc5b2e4d654039ce95ed3ac5a370935575bd67b5
sanghee <wjdtkdgml7352@daum.net>
2022-11-07 14:44:29 +0900

    form 데이터 주고 받기 - 새 글 쓰기 페이지 추가

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,20 @@
+package com.mustache.bbs.domain.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ArticleDto {
+    public ArticleDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    private Long id;
+    private String title;
+    private String content;
+
+
+}
